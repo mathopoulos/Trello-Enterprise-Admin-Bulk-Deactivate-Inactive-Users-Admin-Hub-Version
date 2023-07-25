84b6aa0f52f6187b88082dd1301b13ebbc26e122
Alexandros Mathopoulos <alexandros@mathopoulos.com>
2023-07-25 17:49:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const batchCount = 5;
 
 const testRun = true // if this value is set to true, the script will simulate deactivating inactive users but will not actually deactivate them. Set to false if you would like to actually deactivate users. 
 
-const removeFromEnterprise = false; // if this value is set to true then a user will be deactivated and then also removed from the Enterprise (aka become a Free member)
+const removeFromEnterprise = false; 
+
 
 
 //------------------------------------------------------------------------------------------------------------
@@ -34,6 +35,28 @@ const fs = require('fs');
 const parse = require('csv-parse');
 const timestamp = moment().format("YYYY-MM-DD-HHmmss")
 let pulledBatches = 0; 
+const MAX_RETRIES = 3
+const RETRY_DELAY = 1000; 
+
+// Helper function to handle retries. Set to retry 3 times with a 1000 ms delay between retries. 
+async function withRetry(fn, maxRetries) {
+  let attempts = 0;
+  let error;
+
+  while (attempts <= maxRetries) {
+      try {
+          return await fn();
+      } catch (err) {
+          error = err;
+          attempts++;
+          if (attempts <= maxRetries) {
+              await new Promise(res => setTimeout(res, RETRY_DELAY));
+          }
+      }
+  }
+
+  throw error;
+}
 
 
 
@@ -51,24 +74,31 @@ function putTogetherReport() {
   let getManagedMembersUrl = `https://api.trello.com/1/enterprises/${enterpriseId}/members?fields=idEnterprisesDeactivated,fullName,memberEmail,username,dateLastAccessed&associationTypes=licensed&key=${apiKey}&token=${apiToken}&count=${batchCount}}`;
 
   async function processNextBatch(startIndex) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
     let getNextBatchUrl = `${getManagedMembersUrl}&startIndex=${startIndex}`;
 
+    const requestFn = () => new Promise((innerResolve, innerReject) => {  
     request.get({
       url: getNextBatchUrl,
       headers,
       json: true
     }, async (error, response, body) => {
+      if (error) {
+                    return innerReject(error);
+                }
+      if (response && response.statusCode !== 200) {
+                    return innerReject(new Error(body || `HTTP Error: ${response.statusCode}`));
+                }
       const membersResponse = body;
       pulledBatches = pulledBatches + 1;
       console.log(`Pulled batch #${pulledBatches} with ${membersResponse.length} members. Adding them to the list of users...`);
       if (!Array.isArray(membersResponse) || membersResponse.length === 0) {
         if (testRun === false) {
           console.log(`All members have been added to the report. See member_report_${timestamp}.csv in your directory. Now going to start deactivating inactive users...`);
-          beginDeactivatingUsers();
+          await beginDeactivatingUsers();
         }
         else { console.log(`Test run complete! All members have been added to the report. See member_report_${timestamp}.csv in your directory`) };
-        resolve();
+        innerResolve();
         return;
       }
       membersResponse.forEach((member) => {
@@ -79,88 +109,101 @@ function putTogetherReport() {
         fs.appendFileSync(`pre_run_member_report_${timestamp}.csv`, rowData.join(', ') + '\r\n');
       });
       await processNextBatch(startIndex + batchCount);
-      resolve();
+      innerResolve();
     });
-    });  
-  }
+ });
 
-  processNextBatch(1);
+        try {
+            await withRetry(requestFn, MAX_RETRIES);
+            resolve();
+        } catch (err) {
+            console.error(`Failed to process batch starting from index ${startIndex} after ${MAX_RETRIES} retries. Error: ${err.message}`);
+            reject(err);
+        }
+    });
+}
+
+processNextBatch(1);
+
 }
               
 
-function beginDeactivatingUsers() {
-  const post_timestamp = moment().format("YYYY-MM-DD-HHmmss")
-  // creates csv file where where report will be stored 
-  const post_csvHeaders = [['Member Email', 'Member ID', 'Member Full Name', 'Days Since Last Active', 'Last Active', 'Eligible For Deactivation', 'Deactivated', 'Removed From Enterprise']];
-  fs.writeFileSync(`post_run_member_report_${post_timestamp}.csv`, '');
-  
-  post_csvHeaders.forEach((header) => {
-      fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, header.join(', ') + '\r\n');
-    });
-  
-  // read pre csv file
-  const pre_csvData = fs.readFileSync(`pre_run_member_report_${timestamp}.csv`, "utf-8");
+async function beginDeactivatingUsers() {
+    const post_timestamp = moment().format("YYYY-MM-DD-HHmmss");
 
-  // split csv rows into an array
-  const pre_rows = pre_csvData.trim().split(/\r?\n/);
+    // Create csv file for the report
+    const post_csvHeaders = [['Member Email', 'Member ID', 'Member Full Name', 'Days Since Last Active', 'Last Active', 'Eligible For Deactivation', 'Deactivated', 'Removed From Enterprise']];
 
-  // process each row
-   pre_rows.forEach((pre_row, i) => {
-    const cols = pre_row.split(",");
-    const email = cols[0];
-    const memberId = cols[1].trim();
-    const daysActive = parseInt(cols[3]);
-    
-    const fullName = cols[2];
-    const lastAccessed = cols[4];
-    const isEligible = cols[5];
-    if (daysActive > daysSinceLastActive) {
-      setTimeout(() => {
-        if (!testRun) {
-          const giveEnterpriseSeatUrl = `https://api.trello.com/1/enterprises/${enterpriseId}/members/${memberId}/licensed?key=${apiKey}&token=${apiToken}&value=false`;
-          const data = { memberId:memberId };
-           
-          request.put({
-            url: giveEnterpriseSeatUrl,
-            headers: headers,
-            form: data, 
-          }, (error, response, body) => {
-            if (!error && response.statusCode ===200) {
-              console.log(`Deactivated member: ${fullName} with email ${email}`);
-             if(removeFromEnterprise) {
-              // Now, after successfully deactivating the user, we can remove them from the enterprise
-              const removeFromEnterpriseUrl = `https://api.trello.com/1/enterprises/${enterpriseId}/members/${memberId}/?key=${apiKey}&token=${apiToken}`;
-              request.delete({
-                url: removeFromEnterpriseUrl,
-                headers: headers,
-                form: data, 
-              }, (err, res, body) => {
-                let removedFromEnterprise = "No";
-                if (!err && res.statusCode ===200) {
-                  console.log(`Removed deactivated member from the Enterprise: ${fullName} with email ${email}`);
-                  removedFromEnterprise = "Yes";
-                } else {
-                  console.log(`There was an error removing ${email} from the enterprise: ${body}`)
-                }
-                const rowData = [email, memberId, fullName, daysActive, lastAccessed, isEligible, 'Yes', removedFromEnterprise];
-                fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, rowData.join(', ') + '\r\n');
-              });
-            } else {
-              const rowData = [email, memberId, fullName, daysActive, lastAccessed, isEligible, 'Yes', "No"];
-              fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, rowData.join(', ') + '\r\n');
-            }
-            } else {
-              console.log(`There was an error deactivating ${email}: ${body}`)
-            }
-          });
-        }
-      }, i * 500);
-    } else {
-      const rowData = [email, memberId, fullName, daysActive, lastAccessed, isEligible, 'No'];
-      fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, rowData.join(', ') + '\r\n');
+    try {
+        fs.writeFileSync(`post_run_member_report_${post_timestamp}.csv`, '');
+        post_csvHeaders.forEach((header) => {
+            fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, header.join(', ') + '\r\n');
+        });
+
+        // Read pre-csv file
+        const pre_csvData = fs.readFileSync(`pre_run_member_report_${timestamp}.csv`, "utf-8");
+        var pre_rows = pre_csvData.trim().split(/\r?\n/);
+
+    } catch (err) {
+        console.error('Error while reading or writing CSV:', err);
+        return;
     }
-  });
-};
+
+    const apiRequests = pre_rows.map((pre_row, i) => async () => {
+        const cols = pre_row.split(",");
+        const email = cols[0];
+        const memberId = cols[1].trim();
+        const daysActive = parseInt(cols[3]);
+
+        const fullName = cols[2];
+        const lastAccessed = cols[4];
+        const isEligible = cols[5];
+
+        if (daysActive > daysSinceLastActive) {
+            if (!testRun) {
+                const giveEnterpriseSeatUrl = `https://api.trello.com/1/enterprises/${enterpriseId}/members/${memberId}/licensed?key=${apiKey}&token=${apiToken}&value=false`;
+                const data = { memberId: memberId };
+
+                // This is the wrapped request logic with retries
+                await withRetry(async () => {
+                    const resp = await request.put({
+                        url: giveEnterpriseSeatUrl,
+                        headers: headers,
+                        form: data,
+                        json: true
+                    });
+
+                    // Check if the request was successful
+                    if (resp.statusCode !== 200) {
+                        throw new Error(`Failed to deactivate user: ${resp.body}`);
+                    }
+                    
+                    // Logic to remove the user from the enterprise if required
+                    if (removeFromEnterprise) {
+                        const removeFromEnterpriseUrl = `https://api.trello.com/1/enterprises/${enterpriseId}/members/${memberId}/?key=${apiKey}&token=${apiToken}`;
+                        const res = await request.delete({
+                            url: removeFromEnterpriseUrl,
+                            headers: headers,
+                            form: data,
+                            json: true
+                        });
+
+                        if (res.statusCode !== 200) {
+                            throw new Error(`Failed to remove user from enterprise: ${res.body}`);
+                        }
+                    }
+                }, MAX_RETRIES);
+            }
+        }
+    });
+
+    try {
+        await Promise.all(apiRequests.map(fn => fn()));
+        console.log(`All done! Deactivated all inactive users! You can find the results in post_run_member_report_${post_timestamp}.csv.`);
+    } catch (err) {
+        console.error('Some errors occurred while processing members:', err);
+    }
+}
 
 // run the job once if runOnlyOnce is true, otherwise schedule it to run every X days
 if (runOnlyOnce) {
@@ -176,3 +219,4 @@ if (runOnlyOnce) {
   // run the job once on startup
   putTogetherReport();
 }
+
