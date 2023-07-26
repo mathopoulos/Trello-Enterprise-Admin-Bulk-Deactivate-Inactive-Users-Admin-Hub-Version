814f8cd6656977625f2c2f294578dc563b5862e4
Alexandros Mathopoulos <alexandros@mathopoulos.com>
2023-07-25 21:47:27 -0500

    Fixed a few bugs

--- index.js
+++ index.js
@@ -58,14 +58,11 @@ async function withRetry(fn, maxRetries) {
   throw error;
 }
 
-
-
+// Function to put together pre-report and then kickoff functions to give/reactivate eligiable users. 
 function putTogetherReport() {
-  //creates csv file where where report will be stored 
+  //creates csv file where where report will be test/pre run user report will be stored 
   const csvHeaders = [['Member Email', 'Member ID', 'Member Full Name', 'Days Since Last Active', 'Last Active', 'Eligible For Deactivation']];
-
   fs.writeFileSync(`pre_run_member_report_${timestamp}.csv`, '');
-
   csvHeaders.forEach((header) => {
     fs.appendFileSync(`pre_run_member_report_${timestamp}.csv`, header.join(', ') + '\r\n');
   });
@@ -73,45 +70,58 @@ function putTogetherReport() {
   // API endpoint to get list of Free Members
   let getManagedMembersUrl = `https://trellis.coffee/1/enterprises/${enterpriseId}/members?fields=idEnterprisesDeactivated,fullName,memberEmail,username,dateLastAccessed&associationTypes=licensed&key=${apiKey}&token=${apiToken}&count=${batchCount}}`;
 
+  // Function to pull the next set of users 
   async function processNextBatch(startIndex) {
     return new Promise(async (resolve, reject) => {
-    let getNextBatchUrl = `${getManagedMembersUrl}&startIndex=${startIndex}`;
+        const getNextBatchUrl = `${getManagedMembersUrl}&startIndex=${startIndex}`;
 
-    const requestFn = () => new Promise((innerResolve, innerReject) => {  
-    request.get({
-      url: getNextBatchUrl,
-      headers,
-      json: true
-    }, async (error, response, body) => {
-      if (error) {
+        const requestFn = () => new Promise((innerResolve, innerReject) => {
+            request.get({
+                url: getNextBatchUrl,
+                headers,
+                json: true
+            }, async (error, response, body) => {
+                if (error) {
                     return innerReject(error);
                 }
-      if (response && response.statusCode !== 200) {
+
+                if (response && response.statusCode !== 200) {
                     return innerReject(new Error(body || `HTTP Error: ${response.statusCode}`));
                 }
-      const membersResponse = body;
-      pulledBatches = pulledBatches + 1;
-      console.log(`Pulled batch #${pulledBatches} with ${membersResponse.length} members. Adding them to the list of users...`);
-      if (!Array.isArray(membersResponse) || membersResponse.length === 0) {
-        if (testRun === false) {
-          console.log(`All members have been added to the report. See member_report_${timestamp}.csv in your directory. Now going to start deactivating inactive users...`);
-          await beginDeactivatingUsers();
-        }
-        else { console.log(`Test run complete! All members have been added to the report. See member_report_${timestamp}.csv in your directory`) };
-        innerResolve();
-        return;
-      }
-      membersResponse.forEach((member) => {
-        const daysActive = moment().diff(moment(member.dateLastAccessed), 'days');
-        let eligible = ""
-        if (daysActive > daysSinceLastActive) {eligible = "Yes"} else {eligible = "No"};
-        const rowData = [member.memberEmail, member.id, member.fullName, daysActive, member.dateLastAccessed,eligible];
-        fs.appendFileSync(`pre_run_member_report_${timestamp}.csv`, rowData.join(', ') + '\r\n');
-      });
-      await processNextBatch(startIndex + batchCount);
-      innerResolve();
-    });
- });
+
+                const membersResponse = body;
+                pulledBatches = pulledBatches + 1;
+                console.log(`Pulled batch #${pulledBatches} with ${membersResponse.length} members. Adding them to the list of users...`);
+
+                if (!Array.isArray(membersResponse) || membersResponse.length === 0) {
+                    if (testRun === false) {
+                        console.log(`All members have been added to the report. See member_report_${timestamp}.csv in your directory. Now going to start deactivating inactive users...`);
+                        
+                        await beginGivingSeats();
+                    } else {
+                        console.log(`Test run complete! All members have been added to the report. See member_report_${timestamp}.csv in your directory`);
+                    }
+                    innerResolve();
+                    return;
+                }
+
+                membersResponse.forEach((member) => {
+                    const daysActive = moment().diff(moment(member.dateLastAccessed), 'days');
+                    let eligible = "";
+                    if (daysActive > daysSinceLastActive) {
+                        eligible = "Yes";
+                    } else {
+                        eligible = "No";
+                    }
+                    const rowData = [member.memberEmail, member.id, member.fullName, daysActive, member.dateLastAccessed,eligible];
+                    fs.appendFileSync(`pre_run_member_report_${timestamp}.csv`, rowData.join(', ') + '\r\n');
+                });
+
+                // process next batch recursively
+                await processNextBatch(startIndex + batchCount);
+                innerResolve();
+            });
+        });
 
         try {
             await withRetry(requestFn, MAX_RETRIES);
@@ -127,118 +137,117 @@ processNextBatch(1);
 
 }
               
+// Function that actually gives eligiable users an enterprise seat or re-activates them. 
+async function beginGivingSeats() {
+  const post_timestamp = moment().format("YYYY-MM-DD-HHmmss");
+  
+  const post_csvHeaders = [['Member Email', 'Member ID', 'Member Full Name', 'Days Since Last Active', 'Last Active', 'Eligible For Deactivation', 'Deactivated', 'Removed From Enterprise']];
+  let pre_rows; 
+  try {
+      fs.writeFileSync(`post_run_member_report_${post_timestamp}.csv`, '');
+      post_csvHeaders.forEach((header) => {
+          fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, header.join(', ') + '\r\n');
+      });
+      
+      const pre_csvData = fs.readFileSync(`pre_run_member_report_${timestamp}.csv`, "utf-8");
+      pre_rows = pre_csvData.trim().split(/\r?\n/);
+  } catch (err) {
+      console.error('Error while reading or writing CSV:', err);
+      throw err;
+  }
 
-async function beginDeactivatingUsers() {
-    const post_timestamp = moment().format("YYYY-MM-DD-HHmmss");
+  const apiRequests = pre_rows.map((pre_row, i) => {
+      return new Promise(async (resolve, reject) => {
+          const cols = pre_row.split(",");
+          const email = cols[0];
+          const memberId = cols[1].trim();
+          const daysActive = parseInt(cols[3]);
+          const fullName = cols[2];
+          const lastAccessed = cols[4];
+          const isEligible = cols[5].trim();
 
-    // Create csv file for the report
-    const post_csvHeaders = [['Member Email', 'Member ID', 'Member Full Name', 'Days Since Last Active', 'Last Active', 'Eligible For Deactivation', 'Deactivated', 'Removed From Enterprise']];
-
-    try {
-        fs.writeFileSync(`post_run_member_report_${post_timestamp}.csv`, '');
-        post_csvHeaders.forEach((header) => {
-            fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, header.join(', ') + '\r\n');
-        });
-
-        // Read pre-csv file
-        const pre_csvData = fs.readFileSync(`pre_run_member_report_${timestamp}.csv`, "utf-8");
-        var pre_rows = pre_csvData.trim().split(/\r?\n/);
-
-    } catch (err) {
-        console.error('Error while reading or writing CSV:', err);
-        return;
-    }
-
-    const apiRequests = pre_rows.map((pre_row, i) => async () => {
-        return new Promise(async (resolve, reject) => {
-        const cols = pre_row.split(",");
-        const email = cols[0];
-        const memberId = cols[1].trim();
-        const daysActive = parseInt(cols[3]);
-
-        const fullName = cols[2];
-        const lastAccessed = cols[4];
-        const isEligible = cols[5];
-
-        if (daysActive > daysSinceLastActive) {
-            if (!testRun) {
+          const requestFn = () => new Promise((resolve, reject) => {
+              if (isEligible === "Yes") {
                 const giveEnterpriseSeatUrl = `https://trellis.coffee/1/enterprises/${enterpriseId}/members/${memberId}/licensed?key=${apiKey}&token=${apiToken}&value=false`;
                 const data = { memberId: memberId };
 
-                // This is the wrapped request logic with retries
-                await withRetry(async () => {
-                    const resp = await request.put({
-                        url: giveEnterpriseSeatUrl,
-                        headers: headers,
-                        form: data,
-                        json: true
-                    }, (error, response, body)=>{
-                        if (response.statusCode !== 200) {
-                            throw new Error(`Failed to deactivate user: ${resp.body}`);
-                        } else {
-                            console.log(`Deactivated user ${email}`)
-                            let removedFromEnterprise = "No"
-                            if (removeFromEnterprise) {
-                                const removeFromEnterpriseUrl = `https://trellis.coffee/1/enterprises/${enterpriseId}/members/${memberId}/?key=${apiKey}&token=${apiToken}`;
-                                const res =  request.delete({
-                                    url: removeFromEnterpriseUrl,
-                                    headers: headers,
-                                    form: data,
-                                    json: true
-                                }, (error, response, body)=>{
-                                    if (response.statusCode !== 200) {
-                                        throw new Error(`Failed to remove user from enterprise: ${res.body}`);
-                                    } else {
-                                        console.log(`Removed user from enterprise: ${email}`);
-                                        removedFromEnterprise = "Yes";
-                                        const rowData = [email, memberId, fullName, daysActive, lastAccessed, isEligible, 'Yes', removedFromEnterprise];
-                                        fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, rowData.join(', ') + '\r\n');
-                                    }
+                  request.put({
+                      url: giveEnterpriseSeatUrl,
+                      headers: headers,
+                      form: data,
+                  }, (error, response, body) => {
+                      if (error) {
+                          return reject(error);
+                      }
 
+                      if (response.statusCode === 200) {
+                          console.log(`Deactivated member: ${fullName} with email ${email}`)
+                          let removedFromEnterprise = "No";
+                          if(removeFromEnterprise) {
+                            const removeFromEnterpriseUrl = `https://trellis.coffee/1/enterprises/${enterpriseId}/members/${memberId}/?key=${apiKey}&token=${apiToken}`;
+                            request.delete({
+                                url: removeFromEnterpriseUrl,
+                                headers: headers,
+                                form: data, 
+                              }, (error, response, body) => {
+                                if (error) {
+                                    return reject(error);
+                                    const rowData = [email, memberId, fullName, daysActive, lastAccessed, isEligible, 'Yes', "No"];
+                          fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, rowData.join(', ') + '\r\n');
                                 }
-                                
-                                );
-    
-                            } else {
-                                const rowData = [email, memberId, fullName, daysActive, lastAccessed, isEligible, 'Yes', "No"];
-              fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, rowData.join(', ') + '\r\n');
-                            }
-                        }
-                    }
-                    
-                    );
-                    
-                }, MAX_RETRIES);
-            }
-        } else {
-            const rowData = [email, memberId, fullName, daysActive, lastAccessed, isEligible, 'No'];
-            fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, rowData.join(', ') + '\r\n');
+                                if (response.statusCode === 200) {
+                                  console.log(`Removed deactivated member from the Enterprise: ${fullName} with email ${email}`);
+                                  removedFromEnterprise = "Yes";
+                                  const rowData = [email, memberId, fullName, daysActive, lastAccessed, isEligible, 'Yes', removedFromEnterprise];
+                                fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, rowData.join(', ') + '\r\n');
+                                resolve();
+                                } else {
+                                    const rowData = [email, memberId, fullName, daysActive, lastAccessed, isEligible, 'Yes', "No"];
+                          fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, rowData.join(', ') + '\r\n');
+                                }})
+                          }
+
+                          resolve();
+                      } else {
+                          reject(new Error(body || `HTTP Error: ${response.statusCode}`));
+                      }
+                  });
+              }  else {
+                  const rowData = [email, memberId, fullName, daysActive, lastAccessed, isEligible, 'No', 'No'];
+                  fs.appendFileSync(`post_run_member_report_${post_timestamp}.csv`, rowData.join(', ') + '\r\n');
+                  resolve();
+              }
+          });
+
+          try {
+              await withRetry(requestFn, MAX_RETRIES);
+          } catch (err) {
+              console.error(`Failed to process ${email} after ${MAX_RETRIES} retries. Error: ${err.message}`);
           }
-    });
-    });
 
+          resolve();
+      });
+  });
 
-    try {
-        await Promise.all(apiRequests.map(fn => fn()));
-        console.log(`All done! Deactivated all inactive users! You can find the results in post_run_member_report_${post_timestamp}.csv.`);
-    } catch (err) {
-        console.error('Some errors occurred while processing members:', err);
-    }
-}
+  try {
+      await Promise.all(apiRequests);
+      //console.log(`All done! Deactivated all inactive users! You can find the results in post_run_member_report_${post_timestamp}.csv.`);
+  } catch (err) {
+      console.error('Some errors occurred while processing members:', err);
+  }
+};
 
 // run the job once if runOnlyOnce is true, otherwise schedule it to run every X days
 if (runOnlyOnce) {
-  console.log('Running bulk deactivation script one time only.');
+  console.log('Running bulk licensing script one time only.');
   putTogetherReport();
 
 } else {
-  console.log(`Running bulk deactivation script automatically every ${intervalDays} days`);
+  console.log(`Running bulk licensing script automatically every ${intervalDays} days`);
   cron.schedule(`0 0 1 */${intervalDays} * *`, () => {
-    console.log(`Running bulk deactivation script automatically every ${intervalDays} days`);
+    console.log(`Running bulk licensing script automatically every ${intervalDays} days`);
     putTogetherReport();
   });
   // run the job once on startup
   putTogetherReport();
 }
-
-
